903d52069ccb88345c635a17552d021d27fb0bd4
sajjadaalam12 <sajjadaalam1203@gmail.com>
2025-03-01 22:16:36 +0530

    Add files via upload
    Day 1 Assignments Completed and Uploaded.

---
+++ day1/DataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		int vehicleNo=1825478;
+		boolean isABike=true;
+		float speed=254.547892f;
+		char logo='H';
+		
+		String name="Hero Honda";
+		
+		long phoneNo=9587462145l;
+		double capacity=124.67874565;
+		 
+		String type="3267.624.578.2472";
+		
+		System.out.println(type);		
+		System.out.println(capacity);		  
+		System.out.println(phoneNo);
+		 
+		System.out.println("Vehicle No:- "+vehicleNo+"\n"+"Name of the Vehicle Brand:- "+name+"\n"+"Is the vehicle is bike? "+isABike+"\n"+"Logo of the Vehicle:- "+logo+"\n"+"Speed of the Vehicle:- "+speed);
+		 
+	}
+
+}
